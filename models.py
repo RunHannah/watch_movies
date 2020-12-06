1b17da01bc25dde781125af27e9eb0bd9d8cc20f
Hannah Lee <leexhannah@gmail.com>
2020-12-06 15:11:53 -0800

    display list with image

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 class Movie(db.Model):
     __tablename__ = 'movies'
     id = db.Column(db.Integer, primary_key=True)
+    image = db.Column(db.String(500))
     title = db.Column(db.String(200))
     release_year = db.Column(db.String(4))
     duration = db.Column(db.String(200))
@@ -12,9 +13,19 @@ class Movie(db.Model):
     description = db.Column(db.Text())
 
     # constructor
-    def __init__(self, title, release_year, duration, genre, description):
+    def __init__(self, image, title, release_year, duration, genre, description):
+        self.image = image
         self.title = title
         self.release_year = release_year
         self.duration = duration
         self.genre = genre
         self.description = description
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @staticmethod
+    def get_all():
+        return Movie.query.all()
+        
